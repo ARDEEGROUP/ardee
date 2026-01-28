0b833afc717cd4f14df0390a65d73717f24e3fc6
ARDEEGROUP <webadmin@theardeegroup.com>
2026-01-28 17:36:30 +0530

    Update real-estate.residential.jsx

--- app/routes/real-estate.residential.jsx
+++ app/routes/real-estate.residential.jsx
@@ -57,7 +57,7 @@ export default function RealEstateCommercial() {
           index={0}
         />
       </div>
-<div>upcoming project</div>
+
       <div
         ref={knowMoreTargetRef}
         className="lg:pt-16 lg:-translate-y-16 invisible lg:-mb-16"
