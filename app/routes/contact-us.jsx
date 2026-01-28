cb735e8f57d2bba81f1c6351bcc9f00d3d463ec1
ARDEEGROUP <webadmin@theardeegroup.com>
2026-01-28 15:40:34 +0530

    Update contact-us.jsx

--- app/routes/contact-us.jsx
+++ app/routes/contact-us.jsx
@@ -94,7 +94,7 @@ const ContactUs = () => {
                 </h3>
                 <p className="text-zinc-400">
                   Phone: <a href="tel:911145685035">+91-11-45685035</a> /{" "}
-                  <a href="tel:911141808628">41808628</a>
+                  <a href="tel:9266862777">+919266862777</a>
                 </p>
                 <p className="text-zinc-400">
                   Email:{" "}
