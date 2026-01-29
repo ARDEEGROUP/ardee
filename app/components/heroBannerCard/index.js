b511e67dd29b3eb0bfa04a9d3d9046dfbb4cd763
ARDEEGROUP <webadmin@theardeegroup.com>
2026-01-29 10:29:13 +0530

    Update index.js

--- app/components/heroBannerCard/index.js
+++ app/components/heroBannerCard/index.js
@@ -134,7 +134,7 @@ location.pathname === "/real-estate/residential";
           href="/pdfs/upcoming-projects.pdf"   // <-- your PDF link
           target="_blank"
           rel="noopener noreferrer"
-          classes={"px-7"}
+          className="py-3 border border-white text-white rounded-full hover:bg-black hover:text-white transition"
           
         >
           Upcoming Project
