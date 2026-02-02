404b321c45c16d03e8ad983d8a517fad94138e43
ARDEEGROUP <webadmin@theardeegroup.com>
2026-02-02 15:30:56 +0530

    Update index.js

--- app/components/heroBannerCard/index.js
+++ app/components/heroBannerCard/index.js
@@ -131,7 +131,7 @@ location.pathname === "/real-estate/residential";
                 {/* Anchor button ONLY on residential page */}
       {isResidentialPage && (
         <a
-          href="/pdfs/upcoming-projects.pdf"   // <-- your PDF link
+          href="/images/Ardee-residences-brochure.pdf"   // <-- your PDF link
           target="_blank"
           rel="noopener noreferrer"
             className="py-3 px-7 border border-white text-white rounded-full hover:bg-black hover:text-white transition"
